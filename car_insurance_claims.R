68eb10908da3e32a019158a331010e174e56b6be
find-solace <solaceakello@gmail.com>
2023-12-21 20:04:16 +0300

    imputing missing values

--- car_insurance_claims.R
+++ car_insurance_claims.R
@@ -1,14 +1,42 @@
 #Loading libraries
 library(pacman)
-p_load(tidyverse, ggfortify, glue, yardstick)
+p_load(tidyverse, ggfortify, glue, yardstick, visdat)
 
 #loading and inspecting the df
 insurance_df <- read_csv("car_insurance.csv")
 glimpse(insurance_df)
 
-insurance_df %>% select_if(~any(is.na(.))) %>% summarize_all(~sum(is.na(.)))
-
-##credit score and annual mileage have null values. 
-
 #removing customer id column
 insurance_df <- insurance_df %>% select(-id)
+
+#Examining null values
+insurance_df %>% select_if(~any(is.na(.))) %>% summarize_all(~sum(is.na(.)))
+vis_miss(insurance_df)
+
+##1.1% of the data is missing, in credit_score(10% of the variable) 
+#and annual_mileage(10% of the variable). 
+
+insurance_df %>% mutate(missing_credit_score = is.na(credit_score)) %>%
+  group_by(missing_credit_score) %>% 
+  summarize(across(everything(), median, na.rm = T))
+
+insurance_df %>% mutate(missing_annual_mileage = is.na(annual_mileage)) %>%
+  group_by(missing_annual_mileage) %>% 
+  summarize(across(everything(), median, na.rm = T))
+
+#For missing values in both columns (credit_score and annual_mileage),
+#the median gender is male, while for non-missing values, the median gender is female.
+
+str(insurance_df)
+
+# The structure suggests that the cols with missing values 
+#are normally distributed. Hence, imputing with the mean is acceptable.
+
+
+clean_df <- insurance_df %>% group_by(gender) %>% 
+  mutate(credit_score_filled = 
+           ifelse(is.na(credit_score), mean(credit_score, na.rm = T), credit_score)) %>%
+  mutate(annual_mileage_filled = ifelse(is.na(annual_mileage), mean(annual_mileage, na.rm = T), annual_mileage)) %>%
+  ungroup()
+
+
